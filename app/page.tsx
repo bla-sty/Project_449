fa6bf44a3f5f2998ef517d632c493dd14ddef4b8
pratham120 <20it104@charusat.edu.in>
2025-04-12 00:10:57 -0700

    commit changes
    frontend pages
    navbar
    UI components
    public assets

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+function Homepage() {
+  return(
+    <div>
+    <h1 className="text-3xl">Homepage</h1>
+    <Button variant='outline' size='lg' className='capitalize m-8'>
+      Varun 
+    </Button>
+    </div>
+    );
+}
+
+export default Homepage;
---
+++ components/navbar/NavSearch.tsx
@@ -0,0 +1,6 @@
+import { Input } from "../ui/input";
+
+function NavSearch() {
+    return<Input type="text" placeholder="find a property...." className="max-w-xs dark:bg-muted"></Input> ;
+}
+export default NavSearch;
